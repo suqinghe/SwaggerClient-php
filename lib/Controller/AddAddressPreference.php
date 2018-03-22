3914e10670a358ad1d6c81b4955dd7cce8c715a9
jjxuhude <jjxuhuade@163.com>
2018-03-22 11:22:49 +0800

    update

--- lib/Controller/AddAddressPreference.php
+++ lib/Controller/AddAddressPreference.php
@@ -15,27 +15,32 @@ use Swagger\Client\Model\AddAddressPreferenceRequestData;
 class AddAddressPreference extends AbstractController
 {
     function index(){
-        $token=$this->getAccessToken();
-        $client = new OpenApiApi();
-        
-        $req= new AddAddressPreferenceRequest();
-        $req->setTimestamp(time());
-        $req->setMessageId('11');
-        $req->setEbayId('22');
-        $data=new AddAddressPreferenceRequestData();
-        $data->setType(0);
-        $data->setStreet1('No. 17, 108 Boxing Road, Pudong 401');
-        $data->setName('test Name'.time());
-        $data->setMobile('13300001111');
-        $data->setDistrict('360403'); //浔阳区
-        $data->setCountryCode('CN');
-        $data->setContact('jackxu');
-        $data->setCity('360400');     //九江
-        $data->setPostcode('20000');
-        $data->setProvince('360000');//江西
-        $req->setData($data);
-        $rep =$client->addAddressPreference($token,$req);
-        dump($rep->getData()->getAddressId());
+        try {
+            
+            $token=$this->getAccessToken();
+            $client = new OpenApiApi();
+            
+            $req= new AddAddressPreferenceRequest();
+            $req->setTimestamp(time());
+            $req->setMessageId('11');
+            $req->setEbayId('22');
+            $data=new AddAddressPreferenceRequestData();
+            $data->setType(0);
+            $data->setStreet1('No. 17, 108 Boxing Road, Pudong 401');
+            $data->setName('test Name'.time());
+            $data->setMobile('13300001111');
+            $data->setDistrict('360403'); //浔阳区
+            $data->setCountryCode('CN');
+            $data->setContact('jackxu');
+            $data->setCity('360400');     //九江
+            $data->setPostcode('20000');
+            $data->setProvince('360000');//江西
+            $req->setData($data);
+            $rep =$client->addAddressPreference($token,$req);
+            dump($rep->getData()->getAddressId());
+        } catch (\Exception $e) {
+            $this->getMessage($e);
+        }
         
     }
     
